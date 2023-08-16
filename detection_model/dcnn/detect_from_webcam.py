e891b6a6e8414f8940fef3a9a8f18397ac06aacd
chubachii <huyu_pc@outlook.jp>
2023-08-17 01:40:34 +0900

    update 認識モデルを多クラス分類に変更

--- detection_model/dcnn/detect_from_webcam.py
+++ detection_model/dcnn/detect_from_webcam.py
@@ -9,6 +9,8 @@ cap = cv2.VideoCapture(1)
 while(cap.isOpened()):
 
     _, frame = cap.read()
+    
+    #frame = cv2.imread('test/3.jpg')
 
     img = cv2.resize(frame, dsize=(480,480))
 
@@ -17,14 +19,16 @@ while(cap.isOpened()):
     x = np.expand_dims(x, axis=0)
     x /= 255.0
 
-    model = tf.keras.models.load_model('VGG16_GAP_best.h5')
+    model = tf.keras.models.load_model('machine-status-softmax.h5')
     #model = keras.applications.vgg16.VGG16(include_top=True, weights='imagenet', input_tensor=None, input_shape=None, pooling=None, classes=1000)
     pred = model.predict(x)  
+    
+    print('点灯無し' + str(pred[0][0]*100) + '% ' + '黄' + str(pred[0][1]*100) + '% '+ '赤' + str(pred[0][2]*100) + '% ')
 
-    if pred[0][0] > 0.5:
-        cv2.putText(frame, text='orange: ' + str(round(pred[0][0]*100, 3)) + '%', org=(150, 450), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color=(0, 165, 255), thickness=2)
-    else:
-        cv2.putText(frame, text='red: ' + str(round(100-pred[0][0]*100, 3)) + '%', org=(150, 450), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color=(0, 0, 255), thickness=2)
+    #if pred[0][0] > 0.5:
+        #cv2.putText(frame, text='orange: ' + str(round(pred[0][0]*100, 3)) + '%', org=(150, 450), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color=(0, 165, 255), thickness=2)
+    #else:
+        #cv2.putText(frame, text='red: ' + str(round(100-pred[0][0]*100, 3)) + '%', org=(150, 450), fontFace=cv2.FONT_HERSHEY_SIMPLEX, fontScale=1.5, color=(0, 0, 255), thickness=2)
 
 
     cv2.imshow("Camera", frame)
